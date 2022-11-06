80e5a2c9031487c4a1f09892ac05155b01ebeb42
Viktoriia Moskalenko <v.moskalenkoda@gmail.com>
2022-11-06 16:12:16 +0100

    first commit

---
+++ js.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    var apikey = "LIVDSRZULELA";
+    var lmt = 18;
+  $('.search').click(function(){
+    $(".searched_content").empty();
+    var term = $(".text").val();
+    var url = "https://g.tenor.com/v1/search?q="
+     + term + "&key=" + apikey + "&limit=" + lmt;
+// console.log(url)
+   
+var xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var myArr = JSON.parse(this.responseText);
+    //   console.log(myArr);
+      var i = 0;
+      while(i < myArr["results"].length){
+      var src =  myArr["results"][i]["media"][0]["tinygif"]["url"];
+     $(".searched_content").append("<img class='pic'src="+src+">")
+      i++;
+    }
+    }
+};
+    xhttp.open("GET", url, true);
+    xhttp.send();
+    xhttp.clear();
+  });  
+});
